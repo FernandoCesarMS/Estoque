cb93e1c209f2c27eeb15111aa85622b75d11d58c
FernandoCesarMS <cmnando.silva@gmail.com>
2022-01-25 22:16:42 -0300

    operator > config created

--- Estoque.cpp
+++ Estoque.cpp
@@ -32,4 +32,24 @@ bool operator<(Estoque &lhs, Estoque &rhs)
 
 bool operator>(Estoque &lhs, Estoque &rhs)
 {
+    for (int i = 0; i < rhs.estoque.size(); i++)
+    {
+        bool existe = false, ehMaior = false;
+        for (int j = 0; j < lhs.estoque.size(); j++)
+        {
+            if (rhs.estoque[i].nome == lhs.estoque[j].nome)
+            {
+                existe = true;
+                if (rhs.estoque[i].quantidade < lhs.estoque[j].quantidade)
+                {
+                    ehMaior = true;
+                }
+            }
+        }
+        if (existe == false || ehMaior == false)
+        {
+            return false;
+        }
+    }
+    return true;
 }
